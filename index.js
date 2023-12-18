9d93126e35c947fbb1313dd6835f5cd3456d2bd4
AM10075 <amatamoros@bancatlan.hn>
2023-12-18 11:28:58 -0600

    Changed module types

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import http from "http";
-import express from "express";
-import { Server } from "socket.io";
+const express = require("express");
 const app = express();
-app.get("/", (req,res)=>{
-    res.send("App GET")
-})
-const server = http.createServer(app);
-const io = new Server(server, {
+app.get("/", (req, res) => {
+  res.send("App GET");
+});
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
+
 io.on("connection", (client) => {
   console.log("Client Connected");
   client.on("message", (data) => {
-    console.log(data)
-    client.broadcast.emit("receiveMessage", data.message)
+    console.log(data);
+    client.broadcast.emit("receiveMessage", data.message);
   });
   client.on("disconnect", () => {
-    console.log("Client Disconnected")
+    console.log("Client Disconnected");
   });
 });
 const PORT = process.env.PORT || 4000;
-server.listen(PORT,()=>{
-    console.log("http://localhost:4000")
+server.listen(PORT, () => {
+  console.log("http://localhost:4000");
 });
